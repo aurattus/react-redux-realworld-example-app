b7c953bcb13b27070051de08efebae7667f3474a
Mauricio Saglietto <msaglietto@gmail.com>
2020-08-22 22:04:17 +0200

    Clean up

--- scripts/brdd.js
+++ scripts/brdd.js
@@ -2,7 +2,7 @@ const simpleGit = require("simple-git");
 
 const git = simpleGit();
 
-//git.diff(["--name-only", "HEAD^1"]).then(console.log);
+git.diff(["--name-only", "HEAD^1"]).then(console.log);
 
 const example = `diff --git a/README.md b/README.md
 index 1928820..776ea0a 100644
@@ -16,7 +16,7 @@ index 1928820..776ea0a 100644
  > ### React + Redux codebase containing real world examples (CRUD, auth, advanced patterns, etc) that adheres to the [RealWorld](https://github.com/gothinkster/realworld-example-apps) spec and API.
 
  <a href="https://stackblitz.com/edit/react-redux-realworld" target="_blank"><img width="187" src="https://github.com/gothinkster/realworld/blob/master/media/edit_on_blitz.png?raw=true" /></a>&nbsp;&nbsp;<a href="https://thinkster.io/tutorials/build-a-real-world-react-redux-application" target="_blank"><img width="384" src="https://raw.githubusercontent.com/gothinkster/realworld/master/media/learn-btn-hr.png" /></a>
-- @BRDD 1
+- /* @BRDD 1 */
 diff --git a/package.json b/package.json
 index 9680bc6..37de3e3 100644
 --- a/package.json
@@ -29,7 +29,7 @@ index 9680bc6..37de3e3 100644
 +    "husky": "^4.2.5",
 +    "react-scripts": "1.1.1",
 +    "simple-git": "^2.19.0"
-+    @BRDD 1
++    // @BRDD 1
    },
    "dependencies": {
      "history": "^4.6.3",
@@ -48,10 +48,6 @@ index 9680bc6..37de3e3 100644
 const extractInfo = (diff) => {
   // Detect what files are modified
   const fileChangeRegx = /--- a\/(.*)\s\+\+\+ b\/(.*)/g;
-
-  const match = diff.match(fileChangeRegx);
-  console.log("File Changes: ", match);
-
   const filesLocation = [];
   let result;
 
@@ -61,7 +57,7 @@ const extractInfo = (diff) => {
 
   // Check if there are added tags
   // TODO keep searching until there are no more
-  let index = diff.search(/\+\s+@BRDD/g);
+  let index = diff.search(/\+\s+(\/\/|\/\*)\s+@BRDD/g);
   if (index) {
     // Search in witch file could it be
     const match = filesLocation.find((m) => m.index < index);
@@ -72,7 +68,7 @@ const extractInfo = (diff) => {
   
   // Check if there are removed tags
   // TODO keep searching until there are no more
-  index = diff.search(/\-\s+@BRDD/g);
+  index = diff.search(/\-\s+(\/\/|\/\*)\s+@BRDD/g);
   if (index) {
     // Search in witch file could it be
     const match = filesLocation.find((m) => m.index < index);
